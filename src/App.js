11f1109aabe3f6f18bbb6b9cc7c06b08deb13dec
m-gamao <mmgamao1@gmail.com>
2021-04-01 21:33:12 -0400

    edited routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+// In the switch component, route order matters. Because the switch will
+// only display the first route that is matched to the url.
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import Header from "./components/Header";
-import "./default.scss";
 import Homepage from "./pages/Homepage";
+import Registration from "./pages/Registration";
+import "./default.scss";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="main">
-        <Homepage />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/registration" component={Registration} />
+        </Switch>
       </div>
     </div>
   );

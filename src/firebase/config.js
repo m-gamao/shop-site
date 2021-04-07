4f382e2fff78a39e4d9d7e059198caa400c71564
m-gamao <mmgamao1@gmail.com>
2021-04-07 15:41:24 -0400

    fixed header

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,13 +1,10 @@
-export const firebaseConfig = 
-  var firebaseConfig = {
-    apiKey: "AIzaSyAl8HgjK8Nna4ucyKxwrv5plWdCoe4aglc",
-    authDomain: "ecommerce-site-for-portfolio.firebaseapp.com",
-    projectId: "ecommerce-site-for-portfolio",
-    storageBucket: "ecommerce-site-for-portfolio.appspot.com",
-    messagingSenderId: "1030551232979",
-    appId: "1:1030551232979:web:cc4df146d33452d190cf26",
-    measurementId: "G-7F30JEXSZT"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+export const firebaseConfig = {
+  apiKey: "AIzaSyAl8HgjK8Nna4ucyKxwrv5plWdCoe4aglc",
+  authDomain: "ecommerce-site-for-portfolio.firebaseapp.com",
+  projectId: "ecommerce-site-for-portfolio",
+  storageBucket: "ecommerce-site-for-portfolio.appspot.com",
+  messagingSenderId: "1030551232979",
+  appId: "1:1030551232979:web:cc4df146d33452d190cf26",
+  measurementId: "G-7F30JEXSZT",
+};
+
--- src/firebase/utils.js
+++ src/firebase/utils.js
@@ -3,11 +3,13 @@ import "firebase/firestore";
 import "firebase/auth";
 import { firebaseConfig } from "./config";
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-const = GoogleProvider = new firebase.auth.GoogleAuthProvider();
-GoogleProvider.setCustomParameters({ prompt: 'select_account'});
-export const SignInWithGoogle = () => auth.signInWithPopup(GoogleProvider);
+const GoogleProvider = new firebase.auth.GoogleAuthProvider();
+GoogleProvider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(GoogleProvider);

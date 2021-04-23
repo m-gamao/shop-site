d4844a33f16ad24408911b8c64781d660777b550
m-gamao <mmgamao1@gmail.com>
2021-04-23 13:46:32 -0400

    editing signup index.js

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./styles.scss";
 
+//This form uses the firebase utility file to actually sign up the user.
 import { auth, handleUserProfile } from "./../../firebase/utils";
 
 import FormInput from "./../forms/formInput";
@@ -30,6 +31,18 @@ class Signup extends Component {
     });
   }
 
+  handleFormSubmit = async (event) => {
+    e.preventDefault(); //prevents the page from refreshing when user hits submit.
+    //Next, deconstruct the state into the elements below.
+    const { displayName, email, password, confirmPassword } = this.state;
+
+    //Next is validation.You don't want to proceed any further if the pw and conf pw don't match:
+    if (password !== confirmPassword) {
+      const err = ["Password Don\'t match];
+      
+    }
+  };
+
   render() {
     const { displayName, email, password, confirmPassword } = this.state; //destructure displayName and email from state.
     return (
@@ -37,7 +50,7 @@ class Signup extends Component {
         <div className="wrap">
           <h2>Signup</h2>
           <div className="formWrap">
-            <form>
+            <form onSubmit={this.handleFormSubmit}>
               <FormInput
                 type="text"
                 name="displayName"
--- src/firebase/utils.js
+++ src/firebase/utils.js
@@ -15,15 +15,19 @@ GoogleProvider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(GoogleProvider);
 
 export const handleUserProfile = async (userAuth, additionalData) => {
+  //Takes the userAuth object, and checks if user exists in the users collection in the firebase db.
   if (!userAuth) return;
   const { uid } = userAuth;
   const userRef = firestore.doc("users/${uid}");
   const snapshot = await userRef.get();
   if (!snapshot.exists) {
+    //seeing if user exists
     const { displayName, email } = userAuth;
     const timestamp = new Date();
 
     try {
+      //If user doesn't exist, we register them/create a new document in the users collection,
+      //and we store the information.
       await userRef.set({
         displayName,
         email,
@@ -34,5 +38,6 @@ export const handleUserProfile = async (userAuth, additionalData) => {
       //console.log(err);
     }
   }
-  return userRef;
+  return userRef; //Regardless, we return the userRef document that we can use to store the user's
+  //information in the the local state of the application to sign them in.
 };
